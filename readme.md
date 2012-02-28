72c2c658158f2193f15552ad67e182095161306f
Steve Campbell <dukeytoo@gmail.com>
2012-02-28 11:37:18 -0600

    Basic working redis provider

--- readme.md
+++ readme.md
@@ -4,49 +4,18 @@ This is an implementation of shared memory, for use by multiple node processes t
 
 # Why use this?
 
-You have a node process that you want to scale across CPUs or across servers, but you need to share some data between them.  You could use Redis or MongoDB or memcached, but this is much simpler.
-
-# Is this a database?
-
-No, although in upcoming forks of this project, a database may be used as a means of storing and sharing the memory.
+You have a node process that you want to scale across CPUs or across servers, but you need to share some data between them.  You could use Redis directly, but this is simpler and it is a common API so you can switch to another backend store if you want.
 
 # Usage
 
-In this implementation, you would run node-sharedmem as a service.  You can do so from the command line like:
-
-    $ sharedmem server -p 3000
-
-(will run on port 3000).  See also options for installing as a service or daemon on Windows or Linux: https://github.com/perfectapi/node-perfectapi/wiki/Installers.
-
-Once you have the service running on an endpoint, you can use it in this pattern (you will need to have perfectapi module installed; `npm install perfectapi`):
+In this implementation, you run node-sharedmem-redis as a normal node module. 
 
 ```
-var perfectapi = require('perfectapi');
-perfectapi.proxy('http://localhost:3000/sharedmem', function(err, sharedmem) {
 
-	var myuser = {email: 'steve@perfectapi.com', last:'campbell', first:'steve'};
-    var config = {collection: 'users', 'key': myuser.email, 'variable': myuser};
-    sharedmem.save(config, function(err, result) {
-		//myuser is saved.  Lets check:
-		
-		config = {collection: 'users', 'key': 'steve@perfectapi.com'};
-		sharedmem.get(config, function(err, result) {
-			//result == myuser
-		})
-    });
-
-});
+todo
 ```
-
-You can also access an API test page at http://localhost:3000/sharedmem/testapp (replace localhost with your actual server name).
 
 # Available methods (API)
 
-I have put up a public [test page](http://services.perfectapi.com/sharedmem/testapp/) - it has all the commands and their descriptions.  You can also play around with the library there - just be aware that others may be doing the same.
-
-# Why are the methods asynchronous?
-
-I agree, it is not intuitive to access memory in an asynchronous manner.  On Linux you can use [node-fiberize](https://github.com/lm1/node-fiberize) to switch to synchronous calls.  
-
-I could build fiber support in, but unfortunately node-fibers is not currently available for Windows, (and I want to retain compatibility with Windows).
+I have put up a public [test page](http://services.perfectapi.com/sharedmem/testapp/) - it has all the commands and their descriptions.  You can also play around with the library there - just be aware that others may be doing the same.  The implementation on that page is actually the [node-sharedmem module](https://github.com/perfectapi/node-sharedmem), but the API is the same.
 
---
+++ test/testredis.js
@@ -0,0 +1,142 @@
+var redis = require('../bin/sharedmem.js');
+
+
+describe('Shared redis', function() {
+
+
+  
+  describe('variables', function() {
+    it('save should work without error', function(done) {
+      var data = 'a'
+      var config = {collection: "col 1", variable: data, key: '1'}
+      redis.save(config, function(err, result) {
+        if (err) throw err;
+        
+        done();
+      })      
+    })
+    it('should be retrievable after save', function(done) {
+      var config = {collection: "col 1", key: '1'};
+      redis.get(config, function(err, result) {
+        if (err) throw err;
+        
+        result.should.equal('a');
+        done();
+      })
+    })
+    it('should be in collection after save', function(done) {
+      var config = {collection: "col 1"};
+      redis.getArray(config, function(err, result) {
+        if (err) throw err;
+        
+        result.should.have.property('1', 'a');
+        done();
+      })    
+    })
+    it('should remove from collection after removal', function(done) {
+      var config = {collection: "col 1", key: '1'};
+      redis.remove(config, function(err, result) {
+        if (err) throw err;
+        
+        redis.getArray(config, function(err, result) {
+          if (err) throw err;
+          
+          result.should.not.have.property('1');
+          done();
+        })   
+      })
+    })
+  })
+  
+  describe('timeouts', function() {
+    it('save with timeout should auto-remove after timeout', function(done) {
+      var data = 'a'
+      var config = {collection: "col 2", variable: data, key: '1', options: {expiresIn: 1000} };
+      redis.save(config, function(err, result) {
+        if (err) throw err;
+
+        redis.getArray(config, function(err, result) {
+          //should still be there when we check immediately
+          result.should.have.property('1', 'a');
+          
+          setTimeout(function() {
+            redis.getArray(config, function(err, result) {
+              //should be gone after timeout expires
+              result.should.not.have.property('1');
+            
+              done();
+            })
+         
+
+          }, 1100);
+        })
+      })      
+    })
+    it('can be refreshed by saving the variable again', function(done) {
+      var data = 'a'
+      var config = {collection: "col 3", variable: data, key: '1', options: {expiresIn: 1000} };
+      redis.save(config, function(err, result) {
+        if (err) throw err;
+        
+        config.options.expiresIn = 3000;
+        redis.save(config, function(err, result) {
+          if (err) throw err;
+          
+          //timeout should be 3 now, not 1.  Lets wait 2 and see if the variable is still there
+          setTimeout(function() {
+            redis.getArray(config, function(err, result) {
+              //should be gone after timeout expires
+              result.should.have.property('1', 'a');
+            
+              done();
+            })
+            
+          }, 2000)
+        })
+        
+      })
+      
+    })
+  })
+  
+  describe('counters', function() {
+    it('should be 1 after first increment', function(done) {
+      var config = {counter: 'id'};
+      redis.increment(config, function(err, result) {
+        if (err) throw err;
+        
+        result.should.equal(1);
+        done();
+      })
+    })
+    it('should be 2 after second increment', function(done) {
+      var config = {counter: 'id'};
+      redis.increment(config, function(err, result) {
+        if (err) throw err;
+        
+        result.should.equal(2);
+        done();
+      })
+    }) 
+    it('should go down when decremented', function(done) {
+      var config = {counter: 'id'};
+      redis.decrement(config, function(err, result) {
+        if (err) throw err;
+        
+        result.should.equal(1);
+        done();
+      })    
+    })
+    it('should be -1 when not incremented before first decrement', function(done) {
+      var config = {counter: 'id2'};
+      redis.decrement(config, function(err, result) {
+        if (err) throw err;
+        
+        result.should.equal(-1);
+        done();
+      })    
+    })
+  })
+
+})
+
